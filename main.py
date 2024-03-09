c8ce333a4e03ab81d8fed6fe3c54a756ac0a375b
pdfrar <pedro_f_neto@hotmail.com>
2024-03-09 14:46:09 -0300

    Protótipo de versão que mostra os sites prontos para remover

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 import PySimpleGUI as sg
 global sites
+fecharJanela = False
 sites = []
-try:
-    with open('hosts.txt', 'r') as h:
+exposicaoDeSites = []
+try: #Aqui, o programa inicia verificando se já tem sites adicionado no arquivo, em caso verdadeiro, copia esses sites para uma variável de lista
+    with open('hosts.txt', 'r') as h: #Em caso falso, define a variável para []
         for linha in h:
             linha = linha.replace('127.0.0.1    ', '').replace('\n', '')
             sites.append(linha)
+    for i in range(len(sites)):
+        exposicaoDeSites.append([sg.Checkbox(sites[i], key=f'site{i}')])
 except FileNotFoundError:
     sites = []
-print(f'LerArquivoDeSites: {sites}')
 
 class FrontEnd:
+    def __init__(self):
+        self.layout = self.LayoutDaJanelaParaInserirOsSites() #Aqui o Layout é armazenado em uma variável
+
     def LayoutDaJanelaParaInserirOsSites(self):
-        global layoutinsercaodesites
         layoutinsercaodesites = [
             [sg.Text('Insira aqui o URL do site que deseja bloquear: ', font=("Arial", 11))],
             [sg.Input(size=(45,20),key='siteinserido')],
-            [sg.Button('OK',key='-OK-')]
+            [sg.Button('Apagar selecionados', key='apagarsite')],
+            [sg.Column([sg.Text('Sites bloqueados:')], key='colunadesites')],
+            [sg.Button('OK', key='-OK-')],
+            [sg.Button('Fechar', key ='Fechar')]
         ]
-        janelainsercaodesites = sg.Window('Insira seus sites', layoutinsercaodesites, resizable=True)
-        self.event, self.values = janelainsercaodesites.read()
-        if self.event == '-OK-':
-            janelainsercaodesites.close()
-            sites.append(self.values['siteinserido'])
-        print('{} {}'.format(self.event,self.values['siteinserido']))
+        return sg.Window('Insira seus sites', layoutinsercaodesites, resizable=True)
+
+    def AtualizarJanela(self, window): #Aqui é uma tarefa para atualizar a janela constantemente
+        global fecharJanela 
+        event, values = window.read()
+        if event == sg.WINDOW_CLOSED or event == 'Fechar': #Caso tente fechar a janela seja no X ou no botão Fechar
+            fecharJanela = True
+        elif event == '-OK-':
+            sites.append(values['siteinserido']) #Adiciona o site para a lista
+            for i in range(len(sites)):
+              window['colunadesites'].append([sg.Checkbox(sites[i], key=f'site{i}')])
+            window['siteinserido'].update('') #Reseta o campo de inserção de sites
+            window['colunadesites'.update]
 
 class BackEnd:
     def InputNoArquivoDeTexto(self):
@@ -31,21 +46,20 @@ class BackEnd:
             with open('hosts.txt', 'w') as h:
                 for i in range(len(sites)):
                     h.write(f'127.0.0.1    {sites[i]}\n') #Escreve o site no arquivo hosts.txt
-           # with open('hostsformatado.txt', 'w') as h:
-          #      for i in range(len(sites)):
-           #         h.write('{}\n'.format(sites[i]))
         except FileNotFoundError:
             with open('hosts.txt', 'x') as h:
                 for i in range(len(sites)):
                     h.write(f'127.0.0.1    {sites[i]}\n') #Cria o arquivo e escreve o site no arquivo hosts.txt
-        print('InputNoArquivoDeTexto: ', sites)
-           # with open('hostsformatado.txt', 'x') as h:
-           #     for i in range(len(sites)):
-           #         h.write('{}\n'.format(sites[i]))
+
+frontEnd = FrontEnd()
+backEnd = BackEnd()
 
 while True:
-    FrontEnd().LayoutDaJanelaParaInserirOsSites()
-    BackEnd().InputNoArquivoDeTexto()
+    if fecharJanela == True:
+        break
+    else:
+        frontEnd.AtualizarJanela(frontEnd.layout)
+        backEnd.InputNoArquivoDeTexto()
     #Apertar OK e não ter que fechar a atual para abrir outra janela, e limpar o campo de seleção
     #Printar na janela a lista de sites
     #Depois, tornar a lista útil, de forma que dê para excluir os sites.
