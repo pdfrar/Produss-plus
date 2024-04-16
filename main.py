7e8b96aa600ac0dc8daa45e83d8e3dcc7b123bbe
pdfrar <103325852+pdfrar@users.noreply.github.com>
2024-04-16 00:00:33 -0300

    Lê os sites inseridos, ainda não cria o arquivo.
    Programação completamente comentada.

--- main.py
+++ main.py
@@ -1,65 +1,33 @@
-import PySimpleGUI as sg
-global sites
-fecharJanela = False
-sites = []
-exposicaoDeSites = []
-try: #Aqui, o programa inicia verificando se já tem sites adicionado no arquivo, em caso verdadeiro, copia esses sites para uma variável de lista
-    with open('hosts.txt', 'r') as h: #Em caso falso, define a variável para []
-        for linha in h:
-            linha = linha.replace('127.0.0.1    ', '').replace('\n', '')
-            sites.append(linha)
-    for i in range(len(sites)):
-        exposicaoDeSites.append([sg.Checkbox(sites[i], key=f'site{i}')])
-except FileNotFoundError:
-    sites = []
+import PySimpleGUI as sg #Importa a bilbioteca PySimpleGUI, onde pode foi definido que pode ser chamada como "sg".
+global sites #Define-se a variável sites como global, para ser usada em qualquer função e classe
+sites = ['site1.com', 'site2.com', 'site3.com'] #Define-se valores iniciais para os elementos da variável sites do tipo lista
 
-class FrontEnd:
-    def __init__(self):
-        self.layout = self.LayoutDaJanelaParaInserirOsSites() #Aqui o Layout é armazenado em uma variável
-
-    def LayoutDaJanelaParaInserirOsSites(self):
-        layoutinsercaodesites = [
-            [sg.Text('Insira aqui o URL do site que deseja bloquear: ', font=("Arial", 11))],
-            [sg.Input(size=(45,20),key='siteinserido')],
-            [sg.Button('Apagar selecionados', key='apagarsite')],
-            [sg.Column([sg.Text('Sites bloqueados:')], key='colunadesites')],
-            [sg.Button('OK', key='-OK-')],
-            [sg.Button('Fechar', key ='Fechar')]
+class FrontEnd: #Cria-se a classe para o FrontEnd, a parte visual do programa que irá interagir com o usuário. 
+    def LayoutDaJanelaParaInserirOsSites(self): #Dentro da classe FrontEnd, define-se a tarefa ao lado, com 
+        global layoutinsercaodesites #Define-se a variável sites como global, para ser usada em qualquer função e classe
+        layoutinsercaodesites = [ #Cria uma lista de elementos visuais, oriundos da biblioteca PySimpleGui, para servir como o layout da janela
+            [sg.Text('Insira aqui o URL do site que deseja bloquear: ', font=("Arial", 11))], #Elemento de texto, com fonte Arial 11
+            [sg.Input(size=(45,20),key='siteinserido')], #Elemento de Input (onde o usuário pode digitar), com o tamanho de 45x20 pixeis, com o input sendo salvo na key (que é tipo uma variável) siteinserido
+            [sg.Button('OK',key='-OK-')] #Elemento de botão, com o botão "OK", guardado na key "-OK-"
         ]
-        return sg.Window('Insira seus sites', layoutinsercaodesites, resizable=True)
+        self.janelainsercaodesites = sg.Window('Insira seus sites', layoutinsercaodesites, resizable=True) #O "self" chama a própria função, executando e abrindo a janela com o layout criado
+    
+    def AbrirELerJanelaParaInserirOsSites(self): #Cria-se a tarefa para ler os valores inseridos pelo usuário
+        self.event, self.values = self.janelainsercaodesites.read() #A janela pode retornar dois tipos de entrada, eventos (ex: botões) ou valores (ex: input de texto)
+        print('{} {}'.format(self.event,self.values)) #Essa linha printa os valores resgatados da leitura da janela anteriormente criada.
 
-    def AtualizarJanela(self, window): #Aqui é uma tarefa para atualizar a janela constantemente
-        global fecharJanela 
-        event, values = window.read()
-        if event == sg.WINDOW_CLOSED or event == 'Fechar': #Caso tente fechar a janela seja no X ou no botão Fechar
-            fecharJanela = True
-        elif event == '-OK-':
-            sites.append(values['siteinserido']) #Adiciona o site para a lista
-            for i in range(len(sites)):
-              window['colunadesites'].append([sg.Checkbox(sites[i], key=f'site{i}')])
-            window['siteinserido'].update('') #Reseta o campo de inserção de sites
-            window['colunadesites'.update]
+class BackEnd: #Cria-se uma classe de tarefas para o BackEnd, onde ocorre o processamento dos dados e o usuário não tem acesso
+    def InputNoArquivoDeTexto(self): #Cria-se a tarefa para ler o input do site e salvar em um arquivo de texto
+        try: #A estrutura "try" tenta uma sequência de comandos, esperando um erro.
+            with open('hosts.txt', 'w') as h: #Aqui é o comando para abrir o arquivo em modo de escrita sobre um arquivo já existente, representado pelo 'w'
+                for i in range(len(sites)): #Este for repete pela qtd igual ao tamanho da lista de sites inseridos
+                    h.write('127.0.0.1    {}\n'.format(sites[i])) #Aqui, escreve-se cada linha com o site já no formato do arquivo hosts, com o ip do PC antes do endereço do site.
+        except FileNotFoundError: #Aqui é o erro esperado pela estrutura try. Esse erro ocorre quando tenta-se manipular um arquivo e o mesmo ainda não existe, portanto, utiliza-se essa estrutura para criar o arquivo quando ainda não houver o mesmo
+            with open('hosts.txt', 'x') as h: #Aqui, cria-se o arquivo e depois o abre em modo de escrita.
+                for i in range(len(sites)): #Essas linhas aqui são a mesma lógica da estrutura try acima.
+                    h.write('127.0.0.1    {}}\n'.format(sites[i])) #''
 
-class BackEnd:
-    def InputNoArquivoDeTexto(self):
-        try:
-            with open('hosts.txt', 'w') as h:
-                for i in range(len(sites)):
-                    h.write(f'127.0.0.1    {sites[i]}\n') #Escreve o site no arquivo hosts.txt
-        except FileNotFoundError:
-            with open('hosts.txt', 'x') as h:
-                for i in range(len(sites)):
-                    h.write(f'127.0.0.1    {sites[i]}\n') #Cria o arquivo e escreve o site no arquivo hosts.txt
-
-frontEnd = FrontEnd()
-backEnd = BackEnd()
-
+front_end = FrontEnd() #Cria-se uma instância da classe FrontEnd, passando-a todos os atributos e tarefas da classe.
+front_end.LayoutDaJanelaParaInserirOsSites() #Chama a função da classe FrontEnd, executando-a.
 while True:
-    if fecharJanela == True:
-        break
-    else:
-        frontEnd.AtualizarJanela(frontEnd.layout)
-        backEnd.InputNoArquivoDeTexto()
-    #Apertar OK e não ter que fechar a atual para abrir outra janela, e limpar o campo de seleção
-    #Printar na janela a lista de sites
-    #Depois, tornar a lista útil, de forma que dê para excluir os sites.
+    front_end.AbrirELerJanelaParaInserirOsSites() #Enquanto o usuário não parar o programa de alguma forma, o programa lerá os sites inseridos infinitamente.
