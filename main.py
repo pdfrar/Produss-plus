8cad8c8e83aadd6c4220dc624f7d612171f1bc4c
pdfrar <103325852+pdfrar@users.noreply.github.com>
2024-05-26 19:56:44 -0300

    Completamente preparado pro usuário leigo
    Signed-off-by: pdfrar <103325852+pdfrar@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,69 +1,71 @@
-import PySimpleGUI as sg #Importa a bilbioteca PySimpleGUI, onde pode foi definido que pode ser chamada como "sg".
-global sites #Define-se a variável sites como global, para ser usada em qualquer função e classe
-import socket
+import PySimpleGUI as sg
+import os
 import ctypes
+
+global sites
 global ip
-soma = 0
 global num_linhas
-num_linhas = 0
-sites = [] #Define-se valores iniciais para os elementos da variável sites do tipo lista
+
+sites = []
 linhasparaapagar = []
 sitesparamostrar = []
-class FrontEnd: #Cria-se a classe para o FrontEnd, a parte visual do programa que irá interagir com o usuário. 
-    def LayoutDaJanelaParaInserirOsSites(self): #Dentro da classe FrontEnd, define-se a tarefa ao lado, com 
-        global layoutinsercaodesites #Define-se a variável sites como global, para ser usada em qualquer função e classe
-        layoutinsercaodesites = [ #Cria uma lista de elementos visuais, oriundos da biblioteca PySimpleGui, para servir como o layout da janela
-            [sg.Text('Insira aqui o URL do site que deseja bloquear: ', font=("Arial", 11))], #Elemento de texto, com fonte Arial 11
-            [sg.Input(size=(45,20),key='siteinserido')], #Elemento de Input (onde o usuário pode digitar), com o tamanho de 45x20 pixeis, com o input sendo salvo na key (que é tipo uma variável) siteinserido
-            [sg.Button('ADICIONAR SITE',key='add')],
-            [sg.Button('REMOVER ALGUNS SITES',key='removeralguns')],
-            [sg.Button('BLOQUEAR SITES ADICIONADOS',key='block')],
-            [sg.Button('DESATIVAR BLOQUEIO',key='des')]
-        ]
-        self.janelainsercaodesites = sg.Window('Insira seus sites', layoutinsercaodesites, resizable=True) #O "self" chama a própria função, executando e abrindo a janela com o layout criado
+num_linhas = 0
 
-    def LayoutDaJanelaParaApagarAlgunsSites(self): #Dentro da classe FrontEnd, define-se a tarefa ao lado, com 
+if not os.path.exists('sitesparabloquear.txt'):
+    open('sitesparabloquear.txt', 'w').close()
+
+class FrontEnd:
+    def LayoutDaJanelaParaInserirOsSites(self):
+        global layoutinsercaodesites
+        layoutinsercaodesites = [
+            [sg.Text('Insira aqui o URL do site que deseja bloquear: ', font=("Arial", 11))],
+            [sg.Text('(Recomenda-se copiar e colar o link.)', font=("Arial", 9))],
+            [sg.Input(size=(45, 20), key='siteinserido')],
+            [sg.Button('BLOQUEAR SITE', key='add')],
+            [sg.Button('REMOVER ALGUNS SITES', key='removeralguns')],
+            [sg.Button('DESATIVAR BLOQUEIO', key='des')]
+        ]
+        self.janelainsercaodesites = sg.Window('Produc+: Bloqueador de Sites', layoutinsercaodesites, resizable=True)
+
+    def LayoutDaJanelaParaApagarAlgunsSites(self):
         global sitesparamostrar
-        linhasparaapagar.clear() #ATÉ A LINHA 35 ADICIONEI AGORA PQ ELE NÃO EXCLUÍA SE NÃO TIVESSE NO HOST AINDA
-        for i in range(0, len(sites)):
+        linhasparaapagar.clear()
+        for i in range(len(sites)):
             linhasparaapagar.append(sites[i])
         sitesparamostrar.clear()
         for element in linhasparaapagar:
-            sitesparamostrar.append([sg.Checkbox(f'{element}',key=f'{element}',size=(45,1))])
-        global layoutexclusaodesites #Define-se a variável sites como global, para ser usada em qualquer função e classe
-        layoutexclusaodesites = [ #Cria uma lista de elementos visuais, oriundos da biblioteca PySimpleGui, para servir como o layout da janela
-            [sg.Text('Selecione os sites que deseja excluir da lista de bloqueio: ', font=("Arial", 11))], #Elemento de texto, com fonte Arial 11
-            *[[site] for site in sitesparamostrar],
-            [sg.Button('OK',key='-OK-')]
+            sitesparamostrar.append([sg.Checkbox(f'{element}', key=f'{element}', size=(45, 1))])
+        global layoutexclusaodesites
+        layoutexclusaodesites = [
+            [sg.Text('Selecione os sites que deseja excluir da lista de bloqueio: ', font=("Arial", 11))],
+            *sitesparamostrar,
+            [sg.Button('OK', key='-OK-')]
         ]
-        self.janelaexclusaodesites = sg.Window('Insira seus sites', layoutexclusaodesites, resizable=True) #O "self" chama a própria função, executando e abrindo a janela com o layout criado
+        self.janelaexclusaodesites = sg.Window('Excluir sites', layoutexclusaodesites, resizable=True)
 
-        return self.janelaexclusaodesites
-    
-    def AbrirELerJanelaParaInserirOsSites(self): #Cria-se a tarefa para ler os valores inseridos pelo usuário
-        self.event, self.values = self.janelainsercaodesites.read() #A janela pode retornar dois tipos de entrada, eventos (ex: botões) ou valores (ex: input de texto)
-        if self.event == 'add':
-            spb = open('sitesparabloquear.txt', 'r')
-            print(list(spb))
-            if f'{self.values['siteinserido']}\n' not in spb and self.values['siteinserido'] not in spb and self.values['siteinserido'] != '\n':
-                sites.append(self.values['siteinserido'])
+    def AbrirELerJanelaParaInserirOsSites(self):
+        self.event, self.values = self.janelainsercaodesites.read()
         return self.event
-        
-    
-class BackEnd: #Cria-se uma classe de tarefas para o BackEnd, onde ocorre o processamento dos dados e o usuário não tem acesso
-    
+
+    def AbrirELerJanelaParaApagarAlgunsSites(self):
+        self.event, self.values = self.janelaexclusaodesites.read()
+        return self.event, self.values
+
+class BackEnd:
+    @staticmethod
     def limparHost():
         linhasparaapagar.clear()
-        with open('C:\Windows\System32\drivers\etc\hosts', 'r') as arqhost:
+        with open('C:\\Windows\\System32\\drivers\\etc\\hosts', 'r') as arqhost:
             linhashost = arqhost.readlines()
         for i, linha in enumerate(linhashost):
             if linha[0] != '#':
                 linhasparaapagar.append(i)
-        with open('C:\Windows\System32\drivers\etc\hosts', 'w') as arqhost:
+        with open('C:\\Windows\\System32\\drivers\\etc\\hosts', 'w') as arqhost:
             for i, linha in enumerate(linhashost):
                 if i not in linhasparaapagar:
                     arqhost.write(linha)
 
+    @staticmethod
     def contar_linhas(arq):
         global num_linhas
         num_linhas = 0
@@ -71,70 +73,74 @@ class BackEnd: #Cria-se uma classe de tarefas para o BackEnd, onde ocorre o proc
             for linha in arquivo:
                 num_linhas += 1
         return num_linhas
-    
+
+    @staticmethod
     def sitesiniciais():
         with open('sitesparabloquear.txt', 'r') as arquivo:
             for linha in arquivo:
-                sites.append(linha)
-    
-    def ControleDeOcorrencias(self): #Cria-se a tarefa para ler o input do site e salvar em um arquivo de texto
+                sites.append(linha.strip())
+
+    def ControleDeOcorrencias(self, event):
         global sites
-        #try: #A estrutura "try" tenta uma sequência de comandos, esperando um erro.
-        
-        retorninho = front_end.AbrirELerJanelaParaInserirOsSites()
-
-        if retorninho == 'add':
+        if event == 'add':
             spb = open('sitesparabloquear.txt', 'r')
-            if f'{sites[-1]}\n' not in spb and sites[-1] not in spb:
-                with open('sitesparabloquear.txt', 'a+') as h: #Aqui é o comando para abrir o arquivo em modo de escrita sobre um arquivo já existente, representado pelo 'w'
-                    h.write(f'{sites[-1]}\n') #Aqui, escreve-se cada linha com o site já no formato do arquivo hosts, com o ip do PC antes do endereço do site.
-            sg.popup("Site adicionado à lista de sites para bloquear!")
-        elif retorninho == 'block':
-            BackEnd.limparHost()
-            with open('C:\Windows\System32\drivers\etc\hosts', 'a+') as h:
-                for i in range(0, BackEnd.contar_linhas('sitesparabloquear.txt')): #Essas linhas aqui são a mesma lógica da estrutura try acima.
-                    if i == 0:
-                        h.write('\n127.0.0.1    {}'.format(sites[i]))
-                    else: 
-                        h.write('\n127.0.0.1    {}'.format(sites[i]))
-            sg.popup("Sites bloqueados com sucesso!")
-        elif retorninho == 'des':
-            BackEnd.limparHost()
-            open ('sitesparabloquear.txt', 'w').close()
-            sg.popup('Bloqueio desativado com sucesso!')
-        elif retorninho == 'removeralguns':
+            siteinserido = front_end.values['siteinserido']
+            if f'{siteinserido}\n' not in spb and siteinserido not in spb and siteinserido != '':
+                sites.append(siteinserido)
+                with open('sitesparabloquear.txt', 'a+') as h:
+                    h.write(f'{siteinserido}\n')
+                BackEnd.limparHost()
+                with open('C:\\Windows\\System32\\drivers\\etc\\hosts', 'a+') as h:
+                    for site in sites:
+                        h.write(f'\n127.0.0.1    {site}')
+                sg.popup("Site bloqueado com sucesso!")
+            elif siteinserido == '':
+                sg.popup("Por favor, insira um site para ser bloqueado no campo acima.")
+        elif event == 'des':
+            if sites:
+                BackEnd.limparHost()
+                open('sitesparabloquear.txt', 'w').close()
+                sg.popup('Bloqueio desativado com sucesso!')
+            else:
+                sg.popup('Não há bloqueios para desativar.')
+        elif event == 'removeralguns':
             listadesitesparaapagar = []
-            front_end.LayoutDaJanelaParaApagarAlgunsSites()  # Captura a janela retornada
-            self.event, self.values = front_end.janelaexclusaodesites.read()
-            while self.event != sg.WINDOW_CLOSED and self.event != '-OK-':
-                self.event, self.values = front_end.janelaexclusaodesites.read()
-            for i in range(0, len(sites)):
-                if self.values[sites[i]] == True:
-                    listadesitesparaapagar.append(sites[i])
-            with open ('sitesparabloquear.txt', 'r') as b:
-                linhas = b.readlines()
-            linhas_modificadas = [linha for linha in linhas if linha not in listadesitesparaapagar]
-            with open ('sitesparabloquear.txt', 'w') as b:
-                b.writelines(linhas_modificadas)
-            BackEnd.limparHost()
-            sites.clear()
-            BackEnd.sitesiniciais()
-            with open('C:\Windows\System32\drivers\etc\hosts', 'a+') as h:
-                for i in range(0, BackEnd.contar_linhas('sitesparabloquear.txt')): #Essas linhas aqui são a mesma lógica da estrutura try acima.
-                    if i == 0:
-                        h.write('\n127.0.0.1    {}'.format(sites[i]))
-                    else:
-                        h.write('\n127.0.0.1    {}'.format(sites[i]))
-            front_end.janelaexclusaodesites.close()
-            sg.popup('Sites removidos com sucesso!')
+            front_end.LayoutDaJanelaParaApagarAlgunsSites()
+            if sites:
+                while True:
+                    event, values = front_end.AbrirELerJanelaParaApagarAlgunsSites()
+                    if event == sg.WINDOW_CLOSED or event == '-OK-':
+                        break
+                for site in sites:
+                    if values.get(site, False):
+                        listadesitesparaapagar.append(site)
+                with open('sitesparabloquear.txt', 'r') as b:
+                    linhas = b.readlines()
+                linhas_modificadas = [linha for linha in linhas if linha.strip() not in listadesitesparaapagar]
+                with open('sitesparabloquear.txt', 'w') as b:
+                    b.writelines(linhas_modificadas)
+                BackEnd.limparHost()
+                sites.clear()
+                BackEnd.sitesiniciais()
+                with open('C:\\Windows\\System32\\drivers\\etc\\hosts', 'a+') as h:
+                    for site in sites:
+                        h.write(f'\n127.0.0.1    {site}')
+                front_end.janelaexclusaodesites.close()
+                if listadesitesparaapagar:
+                    sg.popup('Sites removidos com sucesso!')
+            else:
+                sg.popup("Não há sites para remover.")
 
-#Aqui é onde são executadas as tarefas anteriormente criadas
-front_end = FrontEnd() #Cria-se uma instância da classe FrontEnd, passando-a todos os atributos e tarefas da classe.
+front_end = FrontEnd()
 back_end = BackEnd()
 BackEnd.sitesiniciais()
-front_end.LayoutDaJanelaParaInserirOsSites() #Chama a função da classe FrontEnd, executando-a.
+front_end.LayoutDaJanelaParaInserirOsSites()
 
-while ctypes.windll.shell32.IsUserAnAdmin() != 0:
-    back_end.ControleDeOcorrencias()
-
-sg.popup("Você deve executar o programa como administrador.")
+if ctypes.windll.shell32.IsUserAnAdmin() == 0:
+    sg.popup("Você deve executar o programa como administrador.")
+else:
+    while True:
+        event = front_end.AbrirELerJanelaParaInserirOsSites()
+        if event == sg.WINDOW_CLOSED:
+            break
+        back_end.ControleDeOcorrencias(event)
